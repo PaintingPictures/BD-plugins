2282b133d6504eb694cdc2a62d135281bb028509
PaintingPictures <79792342+PaintingPictures@users.noreply.github.com>
2021-02-28 22:06:27 +0100

    ️

--- plugins/AutoStartRichPresence.plugin.js
+++ plugins/AutoStartRichPresence.plugin.js
@@ -3,7 +3,7 @@
 /*
 MIT License
 
-Copyright (c) 2018-2020 Mega_Mewthree
+Copyright (c) 2018-2020 Mega_Mewthree 
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
